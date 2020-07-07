6c5996f15fee4d4c1af2dababd9567cc30a8b048
sasensioe <sasensioe@users.noreply.github.com>
2020-07-07 19:01:00 +0200

    Pages routes added
    path:
    - /home (setted as default)
    - /activities
    - /facilities
    - /contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { FacilitiesComponent } from './pages/facilities/facilities.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"home",       component: HomeComponent },
+  { path:"activities", component: ActivitiesComponent },
+  { path:"facilities", component: FacilitiesComponent },
+  { path:"contact",    component: ContactComponent },
+  { path:"**", pathMatch: "full", redirectTo:"home" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

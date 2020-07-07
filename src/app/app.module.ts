c5d89231148817e20a90146bfb32be0c3cbdabf2
sasensioe <sasensioe@users.noreply.github.com>
2020-07-07 18:55:14 +0200

    Components and modules imported in app.module
    Imports:
    - NavbarComponent
    - FooterComponent
    - HomeComponent
    - ActivitiesComponent
    - FacilitiesComponent
    - ContactComponent
    Material:
    - BrowserAnimationsModule
    - MatExpansionModule
    - MatDividerModule
    - MatListModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { FacilitiesComponent } from './pages/facilities/facilities.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ActivitiesComponent,
+    FacilitiesComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

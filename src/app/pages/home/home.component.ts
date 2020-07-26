429ebd3eaa4b088c4109ac6322e566b9a247568f
sasensioe <sasensioe@users.noreply.github.com>
2020-07-26 19:35:27 +0200

    Home component updated
    - Styles and content updated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+  image: string;
+  link: string;
+}
+
+export interface Plan {
+  name: string;
+  price: string;
+  data: {};
+}
 
 @Component({
   selector: 'app-home',
@@ -8,9 +21,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  tiles: Tile[] = [
+    {text: 'ACTIVITIES', cols: 2, rows: 1, image: 'assets/img/sections/activities.jpg', link:'/activities' },
+    {text: 'FACILITIES', cols: 1, rows: 2, image: 'assets/img/sections/facilities.jpg', link:'/facilities'},
+    {text: 'NEWS', cols: 1, rows: 1, image: 'assets/img/sections/trainers.jpeg', link:'/news'},
+    {text: 'PRICES', cols: 1, rows: 1, image: 'assets/img/sections/nutrition.jpg', link:'/activities'},
+  ];
+
+  plans: Plan[] = [
+    {name: 'GOLD', price: '19,90€/month', data: [
+      'Without permanence', 'Access to all areas', 'Classes included'
+    ]},
+    {name: 'PLATINUM', price: '29,90€/month', data: [
+      'Without permanence', 'Free swimming pool access', 'Access to all areas', 'Classes included', 'Free parking'
+    ]},
+    {name: 'TITANIUM', price: '39,90€/month', data: [
+      'Without permanence', 'Free swimming pool access', 'Access to all areas', 'Classes included', 'Free parking', 'Personal training'
+    ]}
+  ]
+
   constructor() { }
 
+
+
   ngOnInit(): void {
+
+    window.scrollTo(0,0)
+
   }
 
 }

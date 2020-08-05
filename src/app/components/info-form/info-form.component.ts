63f8b52e7a1f94132e344bf96d10b929be447070
sasensioe <sasensioe@users.noreply.github.com>
2020-08-05 18:08:55 +0200

    Info Form component updated
    - Styles added
    - Functions added

--- src/app/components/info-form/info-form.component.ts
+++ src/app/components/info-form/info-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
+import { InfoService } from '../../services/info.service'
 
 @Component({
   selector: 'app-info-form',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoFormComponent implements OnInit {
 
-  constructor() { }
+  @Output() closeInfoForm: EventEmitter<boolean>
 
-  ngOnInit(): void {
+  showForm:boolean;
+
+  formSent:boolean;
+
+  validForm:boolean;
+
+  contactData:FormGroup  
+
+  constructor( private infoService:InfoService ){
+
+    this.contactData = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required]),
+    })
+
+    this.closeInfoForm = new EventEmitter();
+
+    this.showForm = true;
+
+  }
+
+  sendInfo(){
+
+    if(this.contactData.status === 'INVALID'){
+
+      this.validForm = false;
+      this.formSent = true
+      this.showForm = true
+    
+    }else if(this.contactData.status === 'VALID'){
+
+      this.validForm = true;
+      this.formSent = true;
+      this.showForm = false;
+
+      let name = this.contactData.value.name;
+      let email = this.contactData.value.email;
+      let phone = this.contactData.value.phone;
+      let message = this.contactData.value.message;
+      
+      this.infoService.sendInfo(name, email, phone, message);
+
+    }
+
+  }
+
+  close(){
+
+    this.closeInfoForm.emit(false)
+
+  }
+
+  ngOnInit(){
+
   }
 
 }

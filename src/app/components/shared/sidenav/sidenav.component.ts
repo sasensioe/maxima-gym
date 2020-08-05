82f1ef908947f066d8a008d3a7361fbe91d5f963
sasensioe <sasensioe@users.noreply.github.com>
2020-08-05 18:11:27 +0200

    Sidenav updated
    - Styles updated
    - Close sidenav function added

--- src/app/components/shared/sidenav/sidenav.component.ts
+++ src/app/components/shared/sidenav/sidenav.component.ts
@@ -1,35 +1,28 @@
-import { Component, OnDestroy, ChangeDetectorRef } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnDestroy {
+export class SidenavComponent {
 
-  mobileQuery: MediaQueryList;
+@Input() sidenav:boolean;
 
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+@Output() closeSidenav: EventEmitter<boolean>;
 
-  fillerContent = Array.from({length: 50}, () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+constructor(){
 
-  private _mobileQueryListener: () => void;
+  this.closeSidenav = new EventEmitter();
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
+}
 
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+close(){
+
+  this.closeSidenav.emit(false)
+
+}
 
 
 }

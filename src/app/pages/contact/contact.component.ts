8a5e615b7527cb5f0093ef04ee0874850aee0860
sasensioe <sasensioe@users.noreply.github.com>
2020-08-05 18:13:53 +0200

    Contact page updated
    - Styles updated
    - Functions added

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+import { InfoService } from '../../services/info.service'
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  formShow:boolean;
 
-  ngOnInit(): void {
+  formSent:boolean;
+
+  validForm:boolean;
+
+  contactData:FormGroup;
+
+  constructor( private infoService:InfoService ){
+
+    this.contactData = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required]),
+    })
+
+    this.formShow = true;
+
+  }
+
+  sendInfo(){
+
+    if(this.contactData.status === 'INVALID'){
+
+      this.validForm = false;
+      this.formSent = true;
+      this.formShow = true;
+    
+    }else if(this.contactData.status === 'VALID'){
+
+      this.validForm = true;
+      this.formSent = true;
+      this.formShow = false;
+
+      let name = this.contactData.value.name;
+      let email = this.contactData.value.email;
+      let phone = this.contactData.value.phone;
+      let message = this.contactData.value.message;
+      
+      this.infoService.sendInfo(name, email, phone, message);
+
+    }
+
+  }
+
+  ngOnInit() {
+
+    window.scrollTo(0, 0)
+
   }
 
 }

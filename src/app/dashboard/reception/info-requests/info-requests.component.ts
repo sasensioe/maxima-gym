0ed6788d31c954f26309f46a1a06fea3d96a6d95
sasensioe <sasensioe@users.noreply.github.com>
2020-08-14 12:58:30 +0200

    Info requests component added
    - Info requests component added to reception directory

---
+++ src/app/dashboard/reception/info-requests/info-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-requests',
+  templateUrl: './info-requests.component.html',
+  styleUrls: ['./info-requests.component.css']
+})
+export class InfoRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

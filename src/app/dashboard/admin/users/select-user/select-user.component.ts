c412637d276fd028e7a18ff535b571759ec56ff5
sasensioe <sasensioe@users.noreply.github.com>
2020-08-18 13:26:54 +0200

    Select user component updated
    - Styles improved
    - Functions added

--- src/app/dashboard/admin/users/select-user/select-user.component.ts
+++ src/app/dashboard/admin/users/select-user/select-user.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Validators, FormBuilder } from '@angular/forms';
+import { UsersService } from 'src/app/services/users.service';
+import { User } from 'src/app/models/user.model';
+import { SearchService } from 'src/app/services/search.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-select-user',
@@ -9,29 +12,99 @@ import { Validators, FormBuilder } from '@angular/forms';
 })
 export class SelectUserComponent implements OnInit {
 
-  public searchInput = this.formBuilder.group({
-    search: ''
-  })
+  public totalUsers: number;
+  public users: User[] = [];
+  public tempUsers: User[] = [];
+  public from: number = 0;
+  private text:boolean = false;
 
   constructor( private router: Router,
                private route: ActivatedRoute,
-               private formBuilder: FormBuilder) { }
+               private usersService: UsersService,
+               private searchService: SearchService) { }
 
   
 
   ngOnInit(): void {
+
+    this.getUsers()
+
   }
 
+  getUsers(){
+    this.usersService.getUsers(this.from).subscribe(({total, users}) => {
+      this.tempUsers = users;
+      this.users = users;
+      this.totalUsers = total;
+    })
+  }
+
+  changePage(value: number){
+
+    this.from += value;
+
+    if(this.from < 0){
+      this.from = 0;
+    }else if(this.from > this.totalUsers){
+      this.from -= value;
+    }
+
+    this.getUsers()
+
+  }
+  
   goTo(id: number){
 
       this.router.navigate([`../updateUser/${id}`], { relativeTo: this.route });
 
   }
 
-  search(){
+  search(text:string){
 
-    console.log(this.searchInput.controls.search.value)
+    if(text){
+      this.text = true;
+    }else{
+      this.text = false;
+    }
 
+    if(text.length === 0){
+      return this.users = this.tempUsers;
+    }
+
+    this.searchService.search('users', text).subscribe(resp => {
+      this.users = resp;
+      this.totalUsers = resp.length;
+    })
+    console.log(this.text)
+
+  }
+
+
+
+  handlePage(e: PageEvent){
+
+    if(e.pageIndex > e.previousPageIndex){
+      this.from += 5;
+
+      if(!this.text){
+        this.getUsers()
+      }else{
+        return
+      }
+
+    }else{
+      this.from -= 5;
+
+      if(!this.text){
+        this.getUsers()
+      }else{
+        return
+      }
+
+    }
+
+
+    
   }
 
 }

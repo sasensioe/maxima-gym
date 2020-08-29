38cef37f648eb9b0a6f8fd4aa8f5d909c682b2cc
sasensioe <sasensioe@users.noreply.github.com>
2020-08-29 13:20:58 +0200

    Guards updated
    - Update Auth service imports

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../services/dashboard-services/auth.service';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -15,9 +15,6 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
-
-    
-
 
     return this.authService.validateToken().pipe(
       tap(isAuthenticated => {

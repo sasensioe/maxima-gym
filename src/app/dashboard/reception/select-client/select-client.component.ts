070700e2dd6e03eb4421b16a5be1b083065ba4a8
sasensioe <sasensioe@users.noreply.github.com>
2020-09-04 19:59:57 +0200

    Select client component created

---
+++ src/app/dashboard/reception/select-client/select-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-client',
+  templateUrl: './select-client.component.html',
+  styleUrls: ['./select-client.component.css']
+})
+export class SelectClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
